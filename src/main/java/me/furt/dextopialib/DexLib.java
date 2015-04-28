b21f19579e1b2724a0f80b22d68f36f16a5c11ba
Terry Earnheart <lit_69@hotmail.com>
2015-04-28 05:27:27 -0500

    added basic events and logging

--- src/main/java/me/furt/dextopialib/DexLib.java
+++ src/main/java/me/furt/dextopialib/DexLib.java
@@ -1,8 +1,30 @@
 package me.furt.dextopialib;
 
+import org.slf4j.Logger;
+import org.spongepowered.api.event.Subscribe;
+import org.spongepowered.api.event.state.ServerStartedEvent;
+import org.spongepowered.api.event.state.ServerStoppingEvent;
 import org.spongepowered.api.plugin.Plugin;
 
-@Plugin(id = "dextopialib", name = "DexLib", version = "0.1")
-public class DexLib {
+import com.google.inject.Inject;
 
+@Plugin(id = "DexLib", name = "DextopiaLibrary", version = "0.1")
+public class DexLib {
+	
+	@Inject
+	private Logger logger;
+	
+	public Logger getLogger() {
+		return logger;
+	}
+	
+	@Subscribe
+    public void onServerStart(ServerStartedEvent event) {
+		getLogger().info("Plugin Enabled.");
+	}
+	
+	@Subscribe
+	public void onServerStop(ServerStoppingEvent event) {
+		getLogger().info("Plugin Disabled.");
+	}
 }
